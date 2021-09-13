5226105f93751e780ed298efdd35ce9cda6f8972
Sjoerd van der Hoorn <sjoerdvanderhoorn@hotmail.com>
2021-09-13 12:59:56 +0200

    Connections improvements

--- components/relations.js
+++ components/relations.js
@@ -1,6 +1,27 @@
 Vue.component('relations', {
     template: `
         <div class="container">
+
+            <div class="row" style="margin-bottom: 0;">
+                <div class="col s12 offset-m1">
+                    <h1 class="header">
+                        Parents
+                    </h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col s12 offset-m1">
+                    <ul v-if="person.parents && person.parents.length>0" class="collection with-header">
+                        <li v-for="(parent, index) in person.parents" class="collection-header" :key="parent.id">
+                            <a :href="'#/relations/' + parent.id">{{parent.caption}}</a>
+                        </li>
+                    </ul>
+                    <div v-if="!person.parents || person.parents.length==0">
+                        No parents.
+                    </div>
+                </div>
+            </div>
+            
             <div class="row" style="margin-bottom: 0;">
                 <div class="col s12 offset-m1">
                     <h1 class="header">
@@ -23,7 +44,6 @@ Vue.component('relations', {
                                 <a class="secondary-content" v-on:click="unlinkChild(child.id);"><i class="material-icons">link_off</i></a>
                             </li>
                         </ul>
-                        <a class="waves-effect waves-light btn" v-on:click="addChild(index)"><i class="material-icons left">person_add</i>Add child</a>
                     </template>
                     <div v-if="!person.relations || person.relations.length==0">
                         No relations or children. Click the Add button to add a new relation.
@@ -31,69 +51,8 @@ Vue.component('relations', {
                 </div>
             </div>
 
-            <!-- Modals -->
-            <div id="modalAdd" class="modal">
-                <div class="modal-content">
-                    <h4>Add</h4>
-                    <p>
-                        <label>
-                            <input type="radio" value="new" v-model="modalAdd.action" />
-                            <span>Create new person</span>
-                        </label>
-                    </p>
-                    <p>
-                        <label>
-                            <input type="radio" value="link" v-model="modalAdd.action" />
-                            <span>Link with existing person</span>
-                        </label>
-                    </p>
-                    <template v-if="modalAdd.action=='new'">
-                        <h4>New Person</h4>
-                        <div class="row">
-                            <div class="col s6">
-                                <div class="input-field">
-                                    <label for="givenName" class="active">Given Name</label>
-                                    <input id="givenName" v-model="modalAdd.givenName" type="text"/>
-                                </div>
-                            </div>
-                            <div class="col s6">
-                                <div class="input-field">
-                                    <label for="surName" class="active">Surname</label>
-                                    <input id="surName" v-model="modalAdd.surName" type="text"/>
-                                </div>
-                            </div>
-                            <div class="col s6">
-                                <div class="input-field">
-                                    <label for="sex" class="active">Sex</label>
-                                    <input id="sex" v-model="modalAdd.sex" class="autocomplete" type="text"/>
-                                </div>
-                            </div>
-                            <div class="col s12">
-                                <a class="btn waves-effect waves-light modal-close" v-on:click="create();"><i class="material-icons left">person_add</i>Create</a>
-                            </div>
-                        </div>
-                    </template>
-                    <template v-if="modalAdd.action=='link'">
-                        <h4>Existing Person</h4>
-                        <div class="row">
-                            <div class="col s6">
-                                <div class="input-field">
-                                    <i class="material-icons prefix">person</i>
-                                    <input id="existingPerson" type="text" class="autocomplete" placeholder="Find Person" />
-                                </div>
-                            </div>
-                            <div class="col s12">
-                                <a class="btn waves-effect waves-light modal-close" v-on:click="link();"><i class="material-icons left">link</i>Link</a>
-                            </div>
-                        </div>
-                    </template>
-                </div>
-                <div class="modal-footer">
-                    <a class="modal-close waves-effect waves-green btn-flat">Close</a>
-                </div>
-            </div>
             <div style="position: fixed; bottom: 45px; right: 24px;">
-                <button v-on:click="addPartner(person)" class="btn btn-floating btn-large waves-effect waves-light red"><i class="material-icons">add</i></button>
+                <button v-on:click="addPerson()" class="btn modal-trigger btn-floating btn-large waves-effect waves-light red"><i class="material-icons">add</i></button>
             </div>
         </div>
     `,
@@ -127,44 +86,8 @@ Vue.component('relations', {
         }
     },
     methods: {
-        addPartner: function() {
-            this.modalAdd.type = "partner";
-            var modal = M.Modal.init(document.querySelector("#modalAdd"), {
-                "onCloseEnd": this.clearModal
-            });
-            modal.open();
-        },
-        addChild: function(relationIndex) {
-            this.modalAdd.type = "child";
-            this.modalAdd.relationIndex = relationIndex;
-            var modal = M.Modal.init(document.querySelector("#modalAdd"), {
-                "onCloseEnd": this.clearModal
-            });
-            modal.open();
-        },
-        create: function() {
-            if (this.modalAdd.type == "partner") {
-                var newPerson = stamboom.addPerson(this.modalAdd.givenName, this.modalAdd.surName, this.modalAdd.sex);
-                stamboom.addRelation(this.person.id, newPerson.id);
-                stamboom.select(this.person.id);
-            } else if (this.modalAdd.type == "child") {
-                var newPerson = stamboom.addPerson(this.modalAdd.givenName, this.modalAdd.surName, this.modalAdd.sex);
-                var relation = this.person.relations[this.modalAdd.relationIndex];
-                stamboom.addChild(relation.id, newPerson.id)
-            }
-            // Force update
-            stamboom.select(this.person.id);
-        },
-        link: function() {
-            if (this.modalAdd.type == "partner") {
-                stamboom.addRelation(this.person.id, this.modalAdd.person.id);
-            } else if (this.modalAdd.type == "child") {
-                var relation = this.person.relations[this.modalAdd.relationIndex];
-                stamboom.removeChild(this.modalAdd.person.id);
-                stamboom.addChild(relation.id, this.modalAdd.person.id);
-            }
-            // Force update
-            stamboom.select(this.person.id);
+        addPerson: function() {
+            this.$parent.$refs.modalNewPerson.open();
         },
         unlinkChild: function(childId) {
             if (window.confirm("Confirm you want to remove this child from the relation.")) {
@@ -179,9 +102,6 @@ Vue.component('relations', {
                 // Force update
                 stamboom.select(this.person.id);
             }
-        },
-        clearModal: function() {
-            this.modalAdd = {};
         },
         openPerson: function(id) {
             stamboom.select(id);
